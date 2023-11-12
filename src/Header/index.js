fa1f14e25025959d1cb6f323c27f6dc0c8857e89
Spraveen111 <121080561+Spraveen111@users.noreply.github.com>
2023-11-12 15:19:51 +0530

    Update index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,18 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import "./index.css"; // Make sure the path is correct for your index.css file
 
 function Header() {
   return (
     <div className="main-container">
-      <h1 className="logo">E-Buy</h1>
+      <Nav<h1 className="logo">E-Buy</h1>
       <nav className="navbar">
         <ul className="listContainer">
-          <li className="listParts">Home</li>
-          <li className="listParts">Products</li>
-          <li className="listParts">Contact</li>
+          <li className="listParts">
+            <NavLink to="/" activeClassName="active">Home</NavLink>
+          </li>
+          <li className="listParts">
+            <NavLink to="/products" activeClassName="active">Products</NavLink>
+          </li>
+          <li className="listParts">
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+          </li>
         </ul>
       </nav>
       <div className="login-container">
-        <h1>Login</h1>
+        <NavLink to="/login" className="login-link" activeClassName="activeLink">Login</NavLink>
+        <NavLink to="/cart" className="cart-link" activeClassName="activeLink">Cart</NavLink>
       </div>
     </div>
   );
