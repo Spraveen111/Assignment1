ddc072442df98db534a51e40f1f2d96765a2f824
Spraveen111 <121080561+Spraveen111@users.noreply.github.com>
2023-11-15 16:23:49 +0530

    Update index.js

--- src/ProductCard/index.js
+++ src/ProductCard/index.js
@@ -1,24 +1,20 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 import "./index.css";
-import { NavLink } from "react-router-dom";
+
 
 const ProductCard = (props) => {
   const { details } = props;
-  const { title, price, images, id } = details;
-
+  const { title, price, images, id} = details;
+  const valueToSend = id;
   return (
     <li className="productcard">
       <img src={images} alt="product" className="productCardImage" />
       <h1 className="title">{title}</h1>
       <h1 className="price">${price}</h1>
       <div>
-        {/* <Link to={'singleProduct/${id}'}></Link> */}
-        <NavLink to={`singleProduct/${id}`}>
-          <button>display</button>
-        </NavLink>
-        <button type="button" className="button">
-          View{" "}
-        </button>
+  <button type='button'> <a href={`/card/${valueToSend}`}>View</a></button>
+     
+
         <button type="button" className="button">
           AddToCart
         </button>
